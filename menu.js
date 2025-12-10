b92533a60d4de407c0abedf9dae337b73d4e17a3
Yeris Perez <Yerisperez37@gmail.com>
2025-12-10 12:05:39 -0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,32 @@
+const menu = [
+    // Carnes
+    { id: 1, categoria: "Carnes", nombre: "Alitas Fritas", precio: 375, img: "imgs/alitas.jpg" },
+    { id: 2, categoria: "Carnes", nombre: "Muslo Fritas", precio: 375, img: "imgs/muslo.jpg" },
+    { id: 3, categoria: "Carnes", nombre: "Chuletas Fritas", precio: 350, img: "imgs/chuletas.jpg" },
+    { id: 4, categoria: "Carnes", nombre: "Longaniza Artesanal", precio: 350, img: "imgs/longaniza.jpg" },
+    { id: 5, categoria: "Carnes", nombre: "Costillas De Cerdo", precio: 475, img: "imgs/costillas.jpg" },
+    { id: 6, categoria: "Carnes", nombre: "Chicharrón", precio: 500, img: "imgs/chicharron.jpg" },
+    { id: 7, categoria: "Carnes", nombre: "Pescado", precio: 500, img: "imgs/pescado.jpg" },
+    { id: 8, categoria: "Carnes", nombre: "Salami", precio: 240, img: "imgs/salami.jpg" },
+    { id: 9, categoria: "Carnes", nombre: "Mixto", precio: 450, img: "imgs/mixto.jpg" },
+    { id: 10, categoria: "Carnes", nombre: "Bandeja Trío", precio: 1000, img: "imgs/trio.jpg" },
+    { id: 11, categoria: "Carnes", nombre: "Bandeja Familiar", precio: 2000, img: "imgs/familiar.jpg" },
+
+    // Light
+    { id: 12, categoria: "Light", nombre: "Churrasco", precio: 950, img: "imgs/churrasco.jpg" },
+    { id: 13, categoria: "Light", nombre: "Pasta Camarones", precio: 600, img: "imgs/pasta_camarones.jpg" },
+    { id: 14, categoria: "Light", nombre: "Pasta Pechuga", precio: 600, img: "imgs/pasta_pechuga.jpg" },
+    { id: 15, categoria: "Light", nombre: "Pasta Carne Molida", precio: 600, img: "imgs/pasta_carne.jpg" },
+    { id: 16, categoria: "Light", nombre: "Mofongo Camarones", precio: 600, img: "imgs/mofongo_camarones.jpg" },
+    { id: 17, categoria: "Light", nombre: "Mofongo Chicharrón", precio: 500, img: "imgs/mofongo_chicharron.jpg" },
+    { id: 18, categoria: "Light", nombre: "Mofongo Pechuga", precio: 500, img: "imgs/mofongo_pechuga.jpg" },
+    { id: 19, categoria: "Light", nombre: "Pechuga a la Plancha", precio: 425, img: "imgs/pechuga_plancha.jpg" },
+    { id: 20, categoria: "Light", nombre: "Pechuga a la Crema", precio: 475, img: "imgs/pechuga_crema.jpg" },
+    { id: 21, categoria: "Light", nombre: "Camarones a la Criolla", precio: 600, img: "imgs/camarones_criolla.jpg" },
+    { id: 22, categoria: "Light", nombre: "Camarones a la Crema", precio: 600, img: "imgs/camarones_crema.jpg" },
+
+    // Adicionales
+    { id: 23, categoria: "Adicional", nombre: "Servicio de Papa", precio: 125, img: "imgs/papa.jpg" },
+    { id: 24, categoria: "Adicional", nombre: "Servicio de Fritos", precio: 100, img: "imgs/fritos.jpg" },
+    { id: 25, categoria: "Adicional", nombre: "Sopa De Vaso", precio: 100, img: "imgs/sopa.jpg" }
+];
